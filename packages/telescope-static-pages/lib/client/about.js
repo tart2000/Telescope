18bfee5ae53318bdaf81e6ac2b20c1708364a4b7
tart2000 <tart2000design@gmail.com>
2015-03-09 21:41:43 -0400

    now with a banner and a static about page

---
+++ packages/telescope-static-pages/lib/client/about.js
@@ -0,0 +1,13 @@
+Meteor.startup(function () {
+  Router.map(function() {
+    this.route('about', {
+      path: '/about',
+      template: getTemplate('aboutPage')
+    });
+  });
+});
+
+primaryNav.push({
+  template: 'aboutLink',
+  order: 90
+});
