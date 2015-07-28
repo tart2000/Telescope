f742ca971ccd0c2d9c0b3ab2df5901ab2817921d
Arthur Schmitt <tart2000design@gmail.com>
2015-07-28 15:12:55 -0400

    before updating

--- packages/npm-container/package.js
+++ packages/npm-container/package.js
@@ -1,21 +1,26 @@
-  var path = Npm.require('path');                                                        // 86
-  var fs = Npm.require('fs');                                                            // 87
-                                                                                         // 88
-  Package.describe({                                                                     // 89
-    summary: 'Contains all your npm dependencies',                                       // 90
-    version: '1.0.0',                                                                    // 91
-    name: 'npm-container'                                                                // 92
-  });                                                                                    // 93
-                                                                                         // 94
-  var packagesJsonFile = path.resolve('./packages.json');                                // 95
-  try {                                                                                  // 96
-    var fileContent = fs.readFileSync(packagesJsonFile);                                 // 97
-    var packages = JSON.parse(fileContent.toString());                                   // 98
-    Npm.depends(packages);                                                               // 99
-  } catch(ex) {                                                                          // 100
-    console.error('ERROR: packages.json parsing error [ ' + ex.message + ' ]');          // 101
-  }                                                                                      // 102
-                                                                                         // 103
-  Package.onUse(function(api) {                                                          // 104
-    api.add_files(['index.js', '../../packages.json'], 'server');                        // 105
-  });                                                                                    // 106
+var path = Npm.require('path');
+var fs = Npm.require('fs');
+
+Package.describe({
+  summary: 'Contains all your npm dependencies',
+  version: '1.1.0',
+  name: 'npm-container'
+});
+
+var packagesJsonFile = path.resolve('./packages.json');
+try {
+  var fileContent = fs.readFileSync(packagesJsonFile);
+  var packages = JSON.parse(fileContent.toString());
+  Npm.depends(packages);
+} catch (ex) {
+  console.error('ERROR: packages.json parsing error [ ' + ex.message + ' ]');
+}
+
+// Adding the app's packages.json as a used file for this package will get
+// Meteor to watch it and reload this package when it changes
+Package.onUse(function(api) {
+  api.add_files('index.js', 'server');
+  api.add_files('../../packages.json', 'server', {
+    isAsset: true
+  });
+});
